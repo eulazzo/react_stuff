b5e0196f5b64774dedb3cb7660c6d5c54eb0033f
lazaro <lazaromello14@gmail.com>
2020-04-22 17:04:08 -0300

    Conditional output

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -4,47 +4,18 @@ const Ninjas = ({ninjas})=> {
   
   /*Ways to do it */
 
-
-    // const ninjasList = ninjas.map(ninja=>{
-    //   if(ninja.age>10015){
-    //     return (
-          
-    //       <div className="App" key={ ninja._id }>
-    //           <div>Name:{ninja.name}</div>
-    //           <div>Age:{ninja.age}</div>
-    //       </div>
-    //     )
-    //   }else{
-    //     return(
-    //       <div>
-    //          <p>Nennhum usuario encontrado</p>
-    //       </div>
-    //     )
-    //   }
-         
-    // })
-
-    // const ninjasList = ninjas.map(ninja=>{
-    //     return ninja.age<15 ? (
-    //         <div className="App" key={ ninja._id }>
-    //           <div>Name:{ninja.name}</div>
-    //           <div>Age:{ninja.age}</div>
-    //         </div>
-    //   ):null;
-    // })
+    const ninjasList = ninjas.map(ninja=>{
+        return ninja.age<15 ? (
+            <div className="App" key={ ninja._id }>
+              <div>Name:{ninja.name}</div>
+              <div>Age:{ninja.age}</div>
+            </div>
+      ):null;
+    })
     return(
         <div className="ninja-list">
-            {/* { ninjasList  } */}
-            {
-                ninjas.map(ninja=>{
-                  return ninja.age<15 ? (
-                      <div className="App" key={ ninja._id }>
-                        <div>Name:{ninja.name}</div>
-                        <div>Age:{ninja.age}</div>
-                      </div>
-                  ):null;
-                })
-            }
+            { ninjasList  }
+             
         </div>
     );
 
